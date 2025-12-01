7b6a0c0792b849dd51969684c325086531aea765
Jens <jenska@users.noreply.github.com>
2025-12-01 20:56:08 +0100

    Support absolute label operands in parser

--- internal/asm/instructions_test.go
+++ internal/asm/instructions_test.go
@@ -36,6 +36,7 @@ func TestAssembleCoreInstructions(t *testing.T) {
 		{"CmpByte", "CMP.B (16,A0),D2\n", []byte{0xB4, 0x28, 0x00, 0x10}},
 		{"MultiplyWord", "MUL (A1),D0\n", []byte{0xC1, 0xD1}},
 		{"DivideWord", "DIV (A2),D1\n", []byte{0x82, 0xD2}},
+		{"MoveByteLabel", "label:\n.WORD 0\nMOVE.B label,D0\n", []byte{0x00, 0x00, 0x10, 0x39, 0x00, 0x00, 0x00, 0x00}},
 		{"BranchAlwaysShort", "BRA target\n.WORD 0\ntarget:\n", []byte{0x60, 0x02, 0x00, 0x00}},
 		{"BranchConditionWord", "BNE.W target\n.WORD 0\n.WORD 0\ntarget:\n", []byte{0x66, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00}},
 		{"BranchSynonymShort", "BHS target\n.WORD 0\ntarget:\n", []byte{0x64, 0x02, 0x00, 0x00}},
