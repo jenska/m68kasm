f49e2bb50a4190187f265a5bcd08f8cb6916069a
Jens Kaiser <jens.kaiser.de@gmail.com>
2025-12-04 21:09:34 +0100

    scc refactoring

--- internal/asm/instructions/scc.go
+++ internal/asm/instructions/scc.go
@@ -1,35 +1,14 @@
 package instructions
 
 func init() {
-	for _, c := range sccConditions {
-		registerInstrDef(newSccDef(c.Mnemonic, c.Code))
+	for i, c := range sccConditions {
+		registerInstrDef(newSccDef(c, uint16(i)))
 	}
 }
 
-type sccCond struct {
-	Mnemonic string
-	Code     uint16
-}
-
-var sccConditions = []sccCond{
-	{Mnemonic: "ST", Code: 0},
-	{Mnemonic: "SF", Code: 1},
-	{Mnemonic: "SHI", Code: 2},
-	{Mnemonic: "SLS", Code: 3},
-	{Mnemonic: "SCC", Code: 4},
-	{Mnemonic: "SHS", Code: 4},
-	{Mnemonic: "SCS", Code: 5},
-	{Mnemonic: "SLO", Code: 5},
-	{Mnemonic: "SNE", Code: 6},
-	{Mnemonic: "SEQ", Code: 7},
-	{Mnemonic: "SVC", Code: 8},
-	{Mnemonic: "SVS", Code: 9},
-	{Mnemonic: "SPL", Code: 10},
-	{Mnemonic: "SMI", Code: 11},
-	{Mnemonic: "SGE", Code: 12},
-	{Mnemonic: "SLT", Code: 13},
-	{Mnemonic: "SGT", Code: 14},
-	{Mnemonic: "SLE", Code: 15},
+var sccConditions = []string{
+	"ST", "SF", "SHI", "SLS", "SHS", "SLO", "SNE", "SEQ",
+	"SVC", "SVS", "SPL", "SMI", "SGE", "SLT", "SGT", "SLE",
 }
 
 func newSccDef(name string, cond uint16) *InstrDef {
